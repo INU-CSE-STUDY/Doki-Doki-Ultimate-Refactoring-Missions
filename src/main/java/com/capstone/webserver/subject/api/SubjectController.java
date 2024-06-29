c0a2d7b16a8746cb8317b52cd552cd7c2ddbcabe
hg_yellow <jang010505@gmail.com>
2024-06-29 21:29:41 +0900

    refactor: #23 Add major, college get api
    college, major 조회하는 api를 생성하였습니다.

--- src/main/java/com/capstone/webserver/subject/api/SubjectController.java
+++ src/main/java/com/capstone/webserver/subject/api/SubjectController.java
@@ -7,6 +7,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -30,5 +31,21 @@ public class SubjectController {
         List<SubjectDto.SubjectResponseDto> subjects = subjectService.findAll();
 
         return new ResponseEntity<>(subjects, status);
+    }
+
+    @GetMapping("/api/subjects/colleges")
+    public ResponseEntity<List<String>> getColleges() {
+        HttpStatus status = HttpStatus.OK;
+        List<String> colleges = subjectService.getColleges();
+
+        return new ResponseEntity<>(colleges, status);
+    }
+
+    @GetMapping("/api/subjects/colleges/{college}")
+    public ResponseEntity<List<String>> getMajors(@PathVariable(value = "college") String college) {
+        HttpStatus status = HttpStatus.OK;
+        List<String> majors = subjectService.getMajors(college);
+
+        return new ResponseEntity<>(majors, status);
     }
 }
